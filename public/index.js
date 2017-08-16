1a0bced807c804d389187a8c6edb120cdad5bbe0
Fabio Teixeira <fabio1079@gmail.com>
2017-08-16 09:39:21 -0300

    Add webpack

--- public/index.js
+++ public/index.js
@@ -1,5 +1,28 @@
-import './scss/main.css';
+// Import all sass
+require('../src/scss/main.scss');
 
-const logo = require('./logo.svg');
-const Elm = require('./App.elm');
-Elm.App.fullscreen(logo);
+//Import polimer elements
+require("./bower_components/webcomponentsjs/webcomponents-lite.js");
+require("./bower_components/iron-icon/iron-icon.html");
+require("./bower_components/iron-flex-layout/iron-flex-layout-classes.html");
+
+require("./bower_components/paper-button/paper-button.html");
+require("./bower_components/paper-menu-button/paper-menu-button.html");
+require("./bower_components/paper-icon-button/paper-icon-button.html");
+require("./bower_components/paper-fab/paper-fab.html");
+
+require("./bower_components/paper-dropdown-menu/paper-dropdown-menu.html");
+require("./bower_components/paper-menu-button/paper-menu-button.html");
+require("./bower_components/paper-item/paper-item.html");
+require("./bower_components/paper-listbox/paper-listbox.html");
+
+
+
+
+require('./index.html');
+
+var logo = require('./logo.svg');
+var Elm = require('../src/App.elm');
+
+var root = document.getElementById('app-root');
+var app = Elm.App.embed(root, logo);
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+var path = require("path");
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var ELM_PROD_ENV = JSON.parse(process.env.ELM_PROD_ENV || false);
+
+module.exports = {
+    entry: {
+        app: [
+            './public/index.js'
+        ]
+    },
+
+    output: {
+        path: path.resolve(__dirname + '/dist'),
+        filename: '[name].js',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "sass-loader" // compiles Sass to CSS
+                }]
+            },
+            /*
+            {
+                test: /\.(css)$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ]
+            },
+            */
+            {
+                test: /\.html$/,
+                exclude: /node_modules/,
+                loader: 'file-loader?name=[name].[ext]',
+            },
+            {
+                test: /\.elm$/,
+                exclude: [/elm-stuff/, /node_modules/],
+                use: {
+                    loader: 'elm-webpack-loader',
+                    options: {
+                        verbose: true,
+                        warn: true,
+                        debug: !ELM_PROD_ENV
+                    }
+                }
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+            },
+            {
+                test: /\.(ttf|eot|svg|png|jpg|gif)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader',
+            },
+        ],
+
+        noParse: /\.elm$/,
+    },
+
+    devServer: {
+        inline: true,
+        stats: { colors: true },
+    },
+
+    plugins: ELM_PROD_ENV ? [
+        new UglifyJSPlugin()
+    ] : []
+};
