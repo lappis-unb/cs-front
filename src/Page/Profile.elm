a19d35dc860f30de6c551334811e6a1cc9030759
Lucas Malta <lucas50xd@hotmail.com>
2017-09-15 11:05:58 -0300

    making profile screen for unlogged user

--- src/Page/Profile.elm
+++ src/Page/Profile.elm
@@ -4,16 +4,26 @@ import Codeschool.Model exposing (Model)
 import Html exposing (..)
 import Ui.Generic exposing (container)
 import Ui.Parts exposing (promoSimple, promoTable, simpleHero)
+import Html.Attributes exposing (..)
 
 
+
+checkLogin : Model -> Html msg
+checkLogin model =
+  case model.isLogged of
+    True ->
+      div []
+          [ simpleHero model.loggedUser.alias_ "Profile" "simple-hero"
+          , container []
+              [ h1 [] [ text "Personal info" ]
+              , p [] [ text ("Name: " ++ model.loggedUser.alias_) ]
+              , p [] [ text ("E-mail: " ++ model.loggedUser.email) ]
+              ]
+          ]
+    False ->
+      div [ class "main-container" ]
+          [div [class "loggedin-text"] [ text "Please login to see your profile!" ]]
 
 view : Model -> Html msg
 view m =
-    div []
-        [ simpleHero m.loggedUser.alias_ "Profile" "simple-hero"
-        , container []
-            [ h1 [] [ text "Personal info" ]
-            , p [] [ text ("Name: " ++ m.loggedUser.alias_) ]
-            , p [] [ text ("E-mail: " ++ m.loggedUser.email) ]
-            ]
-        ]
+    checkLogin m
