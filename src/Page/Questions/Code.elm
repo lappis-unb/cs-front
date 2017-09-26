24ae5bbbf5c2d70eba108b65bb4474e2d7047dfd
isaque alves de lima <isaquealvesdl@gmail.com>
2017-09-26 18:08:40 -0300

    Changing question code screen. - Creating classes in css - Deleting cancel button

--- src/Page/Questions/Code.elm
+++ src/Page/Questions/Code.elm
@@ -18,18 +18,27 @@ import Ui.Parts exposing (promoSimple, promoTable, simpleHero)
 view : Model -> Html msg
 view m =
     div []
-        [ simpleHero "Idade" "" "simple-hero__page-blue"
-        , div [ class "main-container" ]
+        [ simpleHero "Titulo da questão" "" "simple-hero__page-blue"
+        , div [ class "question-description" ]
             [ text
               """
               Codifique um software receba o ano de nascimento de uma pessoa e o ano atual. Calcule e mostre:
               a) A idade dessa pessoa.
-              b) Quantos anos essa pessoa terá  em 2018.
+              b) Quantos anos essa pessoa terá  em 2018.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+              aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+              aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaa
               """
             ]
+          , select [ class "select-language"  ]
+              [ option [ value "", disabled True, selected True, class "disabled-item" ] [ text "Select language" ]
+              , option [ value "C" ] [ text "C" ]
+              , option [ value "Python" ] [ text "Python" ]
+              , option [ value "Java" ] [ text "Java" ]
+              , option [ value "Java" ] [ text "Java" ]
+              , option [ value "Java" ] [ text "Java" ]
+              ]
           , div [ class "item-question" ]
               [ textarea [ class "item-question", maxlength 1000, placeholder "Insert your code" ] []
               ]
-          , button [ class "cancel-button" ] [ text "Cancel" ]
           , button [ class "send-button" ] [ text "Send to evaluation" ]
         ]
