ee0e42fc1d0ed216f6ff114c79053e1d92c75d60
João Henrique Egewarth <egewarth@gmail.com>
2017-11-28 18:03:31 -0200

    Work in progress
    Signed-off-by: isaque alves de lima <isaquealvesdl@gmail.com>

--- src/Page/Questions/Code.elm
+++ src/Page/Questions/Code.elm
@@ -5,53 +5,14 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Ui.Parts exposing (promoSimple, promoTable, simpleHero)
 import Json.Decode as Json
-import Html.Events.Extra exposing (targetValueMaybeIntParse)
 import Dict
 
 -- import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Ace
 
-
-langItems = Dict.fromList [(Java, "Java"), (Python, "Python 3")]
-langItems = [(Java, "java"), (Python, "python3")]
-langItemsInv = List.map (\(a, b) -> (b, a)) langItems
-
-
-humanLang lang =
-    case Dict.get langItems of
-        Just obj -> obj
-        Nothing -> Python
-
-
-type alias CodeQuestion =
-    { selected : Maybe String }
-
-
-model : CodeQuestion
-model =
-    { selected = Nothing }
-
-
-type Msg
-    = NoOp
-    | Select (Maybe Int)
-
-
-update : Msg -> CodeQuestion -> CodeQuestion
-update msg cls =
-    case msg of
-        NoOp ->
-            cls
-
-        Select s ->
-            { cls | selected = s }
 viewDetail : CodeQuestion -> Html msg
 viewDetail cls =
-    let
-    selectEvent =
-        on "change"
-        (Json.map Select targetSelectedIndex)
 
     div []
         [ simpleHero question.questionInfo.title "" "simple-hero__page-blue"
@@ -64,8 +25,7 @@ viewDetail cls =
         - b) Quantos anos essa pessoa terá  em 2018."""
             ]
 
-        in
-          , select [ class "select-language", selectEvents]
+          , select [ class "select-language"]
               [ option [ value "", disabled True, selected True, class "disabled-item" ] [ text "Select language" ]
               , option [ value "C" ] [ text "C" ]
               , option [ value "Python" ] [ text "Python" ]
@@ -73,7 +33,7 @@ viewDetail cls =
               , option [ value "elm" ] [ text "elm" ]
               , option [ value "Javascript" ] [ text "JavaS" ]
               ]
-          , div [ class "item-question", ]
+          , div [ class "item-question"]
               [ aceEditor "javascript"]
           , button [ class "send-button" ] [ text "Send to evaluation" ]
         ]
@@ -95,7 +55,7 @@ question =
 aceEditor : String -> Html msg
 aceEditor language =
    Ace.toHtml
-      [ Ace.theme "textmate"
+      [ Ace.theme "solarized_dark"
       , Ace.enableBasicAutocompletion True
       , Ace.enableLiveAutocompletion True
       , Ace.tabSize 4
